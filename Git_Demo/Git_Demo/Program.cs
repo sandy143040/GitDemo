f458b1e77a48f2c2deb89e26f71039f4817e823f
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-17 23:34:50 +0530

    [Sandeep] Add: Git demo

---
+++ Git_Demo/Git_Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GitDemoClass gitDemoClass = new GitDemoClass();
+            gitDemoClass.Demo();
+            Console.ReadLine();
+        }
+    }
+}
